c830fb137c4d7aaa02b7245947ab4897a5e0c337
Bruce D'Arcus <bdarcus@gmail.com>
2009-03-27 23:41:13 -0400

    minor additions

--- citeproc.py
+++ citeproc.py
@@ -2,13 +2,16 @@
 import json
 from citeproc.csl.processor import *
 
+"this needs to load a json config file"
+styles_index = json.loads('x')
+
 
 def update_styles():
     """
     Updates the users' style repository.
     """
-    for styles in styles:
-        style.update()
+    for styles in styles_index['styles']:
+        style.update(style['id'])
 
 
 
@@ -17,7 +20,7 @@ def old(style_id):
     Compare the last update date-time-stamp against the server 
     version. 
     """
-    return(style_id['updated'] == get_style_updated(style_id))
+    return(style_id['updated'] == get_style_updated(style_id['link']))
 
 
 def update(style_id):

97bbd6d254b714df5603b494c597cf495931d043
Bruce D'Arcus <bdarcus@gmail.com>
2009-03-27 21:28:19 -0400

    added beginning of parser

---
+++ citeproc/csl_parser.py
@@ -0,0 +1,33 @@
+
+def parse_info(csl):
+    info = Info()
+    return(info)
+
+
+def parse_macros(csl):
+    return([macro for macro in macros])
+
+
+def parse_macro(csl):
+    macro = Macro()
+    return(macro)
+
+
+def parse_citation(csl):
+    citation = Citation()
+    return(citation)
+
+
+def parse_bibliography(csl):
+    bibliography = Bibliography()
+    return(bibliography)
+
+
+def parse_style(csl):
+    info = self.parse_info(csl)
+    macros = self.parse_macros(csl)
+    citation = self.parse_citation(csl)
+    bibliography = self.parse_bibliography(csl)
+    style = Style(info, macros, citation, bibliography)
+    return(style)
+

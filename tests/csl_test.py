8fb0666fffd816dff771ab87ffb0274387953578
Bruce D'Arcus <bdarcus@gmail.com>
2009-04-13 16:28:33 -0400

    begin adding tests

---
+++ tests/csl_test.py
@@ -0,0 +1,30 @@
+
+import json
+from lxml import etree
+#import citeproc
+import glob
+import os
+
+TESTS = glob.glob(os.path.expanduser('~/xbiblio/citeproc-js/branches/fbennett/std/machines/*.json'))
+
+def run_test(test_index, test_path):
+    try:
+        test = json.loads(open(test_path).read())
+        print "\n==================================\n", test['name'], "\n=================================="
+        print "   expecting: ", test['result']
+        try:
+            csl = etree.fromstring(test['csl'])
+            # result = csl.processor.process_citations()
+        except:
+            print "\n    XML parsing failed, using following fragment:"
+            print test['csl']
+    except:
+        print "  ", test_path, "  failed"
+
+
+def run_tests(category='all'):
+    for test_index, test_path in enumerate(TESTS):
+        run_test(test_index, test_path)
+
+
+run_tests()

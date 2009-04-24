90a88ab98d3e7a4f059f75b188248e5b2321c900
Bruce D'Arcus <bdarcus@gmail.com>
2009-04-23 22:10:59 -0400

    minor tweaks

--- tests/csl_test.py
+++ tests/csl_test.py
@@ -10,20 +10,22 @@ TESTS = glob.glob(os.path.expanduser('~/xbiblio/citeproc-js/branches/fbennett/st
 def run_test(test_index, test_path):
     try:
         test = json.loads(open(test_path).read())
-        print(("\n==================================\n", test['name'], "\n=================================="))
-        print(("   expecting: ", test['result']))
+        print("\n==================================\n", test['name'], "\n==================================")
+        print("   expecting: ", test['result'])
         try:
             csl = etree.fromstring(test['csl'])
             # result = csl.processor.process_citations()
         except:
             print("\n    XML parsing failed, using following fragment:")
-            print((test['csl']))
+            print(test['csl'])
     except:
-        print(("  ", test_path, "  failed"))
+        print("  ", test_path, "  failed")
 
 
 def run_tests(category='all'):
+    print("There are ", len(TESTS), " tests to run.")
     for test_index, test_path in enumerate(TESTS):
+        print("test: ", test_index, " ", test_path)
         run_test(test_index, test_path)
 
 

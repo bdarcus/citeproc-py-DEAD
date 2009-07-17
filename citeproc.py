d70338a403be849f1ff05091fbd3e08bde946210
Bruce D'Arcus <bdarcus@gmail.com>
2009-07-16 21:14:23 -0400

    more on process_choose

--- citeproc.py
+++ citeproc.py
@@ -113,18 +113,19 @@ def process_choose(style_node, reference):
     """
     When given a style node and a reference, return an evaluated cs:choose.
     """
-    if condition(style_node.find(CSLNS + "if").attrib):
+    _if = style_node.find(CSLNS + "if")
+    _elif = style_node.findall(CSLNS + "else-if")
+    _else = style_node.find(CSLNS + "else")
+
+    if condition(_if.attrib):
         pass
     
-    for elseif in style_node.findall(CSLNS + "else-if"):
-        elseif = style_node.find(CSLNS + "else-if")
-        elif condition(elseif.attrib):
+    for elseif in _elif:
+#        elif condition(elseif.attrib):
             pass
 
-    if style_node.find(CSLNS + "else"):
-        _else = style_node.find(CSLNS + "else")
-        if condition(_else.attrib):
-            pass
+    if _else and condition(_else.attrib):
+        pass
 
 def process_text(parent, style_node, style_macros, reference):
     """

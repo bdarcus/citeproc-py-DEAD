27ed0ebe6e9b98e18627415149d1ca1a18b0374b
Bruce D'Arcus <bdarcus@gmail.com>
2009-09-01 23:19:09 -0400

    whitespace cleaning

--- parser.py
+++ parser.py
@@ -2,12 +2,14 @@ from pyparsing import *
 
 DOC = "One [doe99] two [smith09; jones08]. Three [zon05@23]."
 
+CITED = []
+
 def parse_citations(document):
-    lbracket = Suppress( "[" )
-    rbracket = Suppress( "]" )
+    lbracket = Suppress("[")
+    rbracket = Suppress("]")
     digits = "0123456789"
     key = Group(Word(alphanums) + Optional(Suppress("@")) + Optional(Word(digits)))
-    citation = lbracket + delimitedList(key, ';') + rbracket
+    citation = lbracket + delimitedList(key, ";") + rbracket
 
     return(citation.searchString(document))
 

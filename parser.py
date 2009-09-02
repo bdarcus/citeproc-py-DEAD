0315b600567fd24a41d46c05c398256866d15851
Bruce D'Arcus <bdarcus@gmail.com>
2009-09-01 23:09:20 -0400

    working on locator parsing

--- parser.py
+++ parser.py
@@ -1,13 +1,12 @@
 from pyparsing import *
 
-DOC = "One [doe99] two [smith09; jones08]. Three [zon05]."
+DOC = "One [doe99] two [smith09; jones08]. Three [zon05@23]."
 
 def parse_citations(document):
     lbracket = Suppress( "[" )
     rbracket = Suppress( "]" )
-
-    key = Word(alphanums)
-
+    digits = "0123456789"
+    key = Group(Word(alphanums) + Optional(Suppress("@")) + Optional(Word(digits)))
     citation = lbracket + delimitedList(key, ';') + rbracket
 
     return(citation.searchString(document))
